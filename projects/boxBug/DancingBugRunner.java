90b2aed7d1f1ba5d1bcf1798d50e06d82815abd1
Eddie Villa <edward.villasenor@gmail.com>
2026-03-26 21:48:10 -0500

    finished abstracting drawing of dragon curve away to just providing number of iterations

--- projects/boxBug/DancingBugRunner.java
+++ projects/boxBug/DancingBugRunner.java
@@ -21,6 +21,7 @@ import info.gridworld.grid.Location;
 import info.gridworld.grid.UnboundedGrid;
 
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * This class runs a world that contains box bugs. <br />
@@ -28,22 +29,53 @@ import java.awt.*;
  */
 public class DancingBugRunner
 {
-    public static void main(String[] args)
-    {
-        ActorWorld world = new ActorWorld(new UnboundedGrid<>());
-//        int[] aliceMoves = {1,3,1,2};
-//        DancingBug alice = new DancingBug(aliceMoves);
-//        alice.setColor(Color.ORANGE);
-        // 1 is a left turn. 0 is a right turn
-        // dragon curve: start with 1. next iteration is append a 1 and then repeat your last iteration but with the middle digit complemented. So: (1) 1 (0) which gives us 110. Next iteration is (110)1(100)
-//        int[] bobMoves = {0,  2,2,6,2,2,6,6,2,2,2,6,6,2,6,6};
-//        int[] bobMoves = {2,2,6,2,2,6,6};
-        int[] bobMoves = {2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,2,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,2,2,2,6,6,2,6,6,6,2,2,6,2,2,6,6,6,2,2,6,6,2,6,6};
+    public static int[] getDragonCurveArray(int iterations) {
+        iterations -= 1;
+        String start = "1";
+        int[] result;
+        int i;
+        String tmp;
+        for (int itr=0;itr<iterations;itr++) {
+            i = start.length() / 2;
+            tmp = start + "1" + start.substring(0, i) + String.valueOf((Integer.parseInt(start.substring(i, i + 1)) + 1) % 2) + start.substring(i + 1);
+            start = tmp;
+        }
+        start = start.replace("1", "2,").replace("0","6,");
+        start = start.substring(0,start.length()-1);
+        result = Arrays.stream(start.split(","))
+                .map(String::trim)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return result;
+    }
 
+    /**
+     * 1 is a left turn. 0 is a right turn
+     * dragon curve: start with 1. next iteration is append a 1 and then repeat your
+     * last iteration but with the middle digit complemented. So: (1) 1 (0) which
+     * gives us 110. Next iteration is (110)1(100)
+     *
+     * @param dmoves are the dance moves for the bug
+     * @return nothing
+     */
+    public static void runGrid(int[] dmoves) {
+        ActorWorld world = new ActorWorld(new UnboundedGrid<>());
+        int[] bobMoves = dmoves;
         DancingBug bob = new DancingBug(bobMoves);
         bob.setDirection(180);
-//        world.add(new Location(7, 8), alice);
         world.add(new Location(5, 5), bob);
         world.show();
+    }
+    public static void main(String[] args)
+    {
+        //test1
+        System.out.println("test1");
+        // length is 2^(iterations)-1
+        for (int elt : getDragonCurveArray(2)) {
+            System.out.print(elt);
+        }
+
+        //gui visual test
+        runGrid(getDragonCurveArray(9));
     }
 }
